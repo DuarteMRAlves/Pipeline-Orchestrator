2b1d42ce1e3f3fb8b26826f8b1d30be22ba8852f
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2021-03-29 20:57:33 +0100

    Setup integration tests
      * Closes #4: Created directory with integration tests and integrationTests task

---
+++ app/src/integration/java/pipeline/orchestrator/AppIT.java
@@ -0,0 +1,13 @@
+package pipeline.orchestrator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppIT {
+
+    @Test
+    public void simpleTest() {
+        assertTrue(true);
+    }
+}
